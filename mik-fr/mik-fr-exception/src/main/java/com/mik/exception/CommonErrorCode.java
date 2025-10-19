555849f53136f9c6e7639914b588e1dc34b26027
lin819747263 <819747263@qq.com>
2025-10-19 17:10:17 +0800

    参数校验增加
    Signed-off-by: lin819747263 <819747263@qq.com>

--- mik-fr/mik-fr-exception/src/main/java/com/mik/exception/CommonErrorCode.java
+++ mik-fr/mik-fr-exception/src/main/java/com/mik/exception/CommonErrorCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum CommonErrorCode implements ErrorCode {
     UnKnowException(999, "未知错误,请联系管理员"),
-    TooManyRequests(409, "Too Many Requests");
+    TooManyRequests(409, "Too Many Requests"),
+    paramError(10001, "Too Many Requests");
 
     private Integer code;
     private String message;
--- mik-fr/mik-fr-mvc/src/main/java/com/mik/mvc/exception/GlobalExceptionHandler.java
+++ mik-fr/mik-fr-mvc/src/main/java/com/mik/mvc/exception/GlobalExceptionHandler.java
@@ -3,10 +3,14 @@ package com.mik.mvc.exception;
 import com.mik.core.constant.ResultCode;
 import com.mik.core.exception.ServiceException;
 import com.mik.core.pojo.Result;
+import com.mik.exception.CommonErrorCode;
 import feign.FeignException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,23 +18,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class, ConstraintViolationException.class, BindException.class})
+    public Object paramExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+        log.debug("参数异常", ex);
+        return Result.error(CommonErrorCode.paramError);
+    }
+
     @ExceptionHandler(FeignException.class)
     public Object feignExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        log.error("业务异常", ex);
+        log.debug("业务异常", ex);
         FeignException exception = (FeignException)ex;
         return Result.error(1, exception.getMessage());
     }
 
     @ExceptionHandler(ServiceException.class)
     public Object serviceExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        log.error("业务异常", ex);
+        log.debug("业务异常", ex);
         ServiceException exception = (ServiceException)ex;
         return Result.error(exception.getErrorCode(), exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public Object undeclaredThrowableExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        log.error("未处理异常", ex);
+        log.debug("未处理异常", ex);
         return Result.error(ResultCode.UNKNOW.getCode(), ex.getMessage());
     }
 
