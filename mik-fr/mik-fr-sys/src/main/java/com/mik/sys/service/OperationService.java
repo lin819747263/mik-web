0a3c08239c402fa31d31244738c2287f1c3987f1
lin819747263 <819747263@qq.com>
2025-10-19 17:52:50 +0800

    增加日志定期删除
    Signed-off-by: lin819747263 <819747263@qq.com>

--- mik-fr/mik-fr-sys/src/main/java/com/mik/sys/service/OperationService.java
+++ mik-fr/mik-fr-sys/src/main/java/com/mik/sys/service/OperationService.java
@@ -2,11 +2,35 @@ package com.mik.sys.service;
 
 import com.mik.sys.entity.OperationLogEntity;
 import com.mik.sys.mapper.OperationMapper;
+import com.mybatisflex.core.query.QueryColumn;
+import com.mybatisflex.core.query.QueryCondition;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 public class OperationService extends ServiceImpl<OperationMapper, OperationLogEntity> {
+
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void clearLog(){
+        // 获取当前日期
+        LocalDate now = LocalDate.now();
+
+        // 减去一个月
+        LocalDate lastMonth = now.minusMonths(1);
+
+        // 定义格式器
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // 转为字符串
+        String result = lastMonth.format(formatter);
+        getMapper().deleteByCondition(QueryCondition.create(new QueryColumn("create_time"), "<=", result));
+    }
 
 }
--- mik-user/user-service/src/main/java/com/mik/UserApplication.java
+++ mik-user/user-service/src/main/java/com/mik/UserApplication.java
@@ -3,8 +3,10 @@ package com.mik;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan("com.mik.**.mapper")
 public class UserApplication {
     public static void main(String[] args) {
